1d5429942c034551369def15d2fa470028c4e4aa
Satyam Bansal <satyamsgsits1994@gmail.com>
2025-07-30 11:37:09 +0530

    Config and package json changes

---
+++ relayer/src/types.d.ts
@@ -0,0 +1,70 @@
+export interface Point {
+  delay: number;
+  coefficient: number;
+}
+
+export interface GasCost {
+  gasBumpEstimate: number;
+  gasPriceEstimate: string;
+}
+
+export interface Preset {
+  auctionDuration: number;
+  startAuctionIn: number;
+  initialRateBump: number;
+  auctionStartAmount: string;
+  startAmount: string;
+  auctionEndAmount: string;
+  exclusiveResolver: null;
+  costInDstToken: string;
+  points: Point[];
+  allowPartialFills: boolean;
+  allowMultipleFills: boolean;
+  gasCost: GasCost;
+  secretsCount: number;
+}
+
+export interface TimeLocks {
+  srcWithdrawal: number;
+  srcPublicWithdrawal: number;
+  srcCancellation: number;
+  srcPublicCancellation: number;
+  dstWithdrawal: number;
+  dstPublicWithdrawal: number;
+  dstCancellation: number;
+}
+
+export interface Prices {
+  usd: {
+    srcToken: string;
+    dstToken: string;
+  };
+}
+
+export interface Volume {
+  usd: {
+    srcToken: string;
+    dstToken: string;
+  };
+}
+
+export interface QuoterResponse {
+  quoteId: string;
+  srcTokenAmount: string;
+  dstTokenAmount: string;
+  autoK: number;
+  presets: {
+    fast: Preset;
+    medium: Preset;
+    slow: Preset;
+  };
+  timeLocks: TimeLocks;
+  srcEscrowFactory: string;
+  dstEscrowFactory: string;
+  srcSafetyDeposit: number;
+  dstSafetyDeposit: number;
+  whitelist: string[];
+  recommendedPreset: string;
+  prices: Prices;
+  volume: Volume;
+}
