b0417876b279b950d24d3c3ca7dfe48c6fcf5735
Satyam Bansal <satyamsgsits1994@gmail.com>
2025-07-31 17:01:34 +0530

    eth signature changes

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,31 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { EIP712TypedData } from '@1inch/cross-chain-sdk'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const requestEthereumSignature = async (typedData: EIP712TypedData, userAddress: string) => {
+  const signature = await window.ethereum.request({
+    method: 'eth_signTypedData_v4',
+    params: [
+      userAddress,
+      JSON.stringify({
+        domain: typedData.domain,
+        types: {
+          EIP712Domain: [
+            { name: 'name', type: 'string' },
+            { name: 'version', type: 'string' },
+            { name: 'chainId', type: 'uint256' },
+            { name: 'verifyingContract', type: 'address' },
+          ],
+          ...typedData.types,
+        },
+        primaryType: typedData.primaryType,
+        message: typedData.message,
+      }),
+    ],
+  })
+  return signature
 }
